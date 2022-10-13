938756548825b9fa89306603575072fb2d22ba21
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2022-10-13 08:29:55 +0530

    Made it a bit more complex

---
+++ Co.bat
@@ -0,0 +1,2 @@
+@echo off
+for /D %%a in ("C:\Users\eippe\OneDrive\Desktop\Directory\subdirectory7\subdirectory6\*.*") do xcopy /y /d C:\Users\eippe\OneDrive\Desktop\Directory\subdirectory\README.md "%%a\"
